c9fc25c9bcfeb8227be281a142f76655112fab70
Miguel Fortuna <miguelfortuna98@gmail.com>
2020-06-01 22:08:26 +0800

    new changes

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ function printText(id, sentence, speed)
 {
     var index = 0,
         timer = setInterval(function() {
-        var char= sentence.charAt(index);
+        var char = sentence.charAt(index);
         if(char === '<') index= sentence.indexOf('>',index);
         document.getElementById(id).innerHTML= sentence.substr(0,index);
         if(++index === sentence.length){
