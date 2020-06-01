4d370bbeed98ab1d0c94f703a6f8295ae2b7cab0
Miguel Fortuna <miguelfortuna98@gmail.com>
2020-06-01 13:34:44 +0800

    new changes

--- js/index.js
+++ js/index.js
@@ -1,20 +1,32 @@
-// function printBasicInfo(id, sentence, speed) 
-// {
-//     var index = 0,
-//         timer = setInterval(function() {
-//         var char= sentence.charAt(index);
-//         if(char === '<') index= sentence.indexOf('>',index);
-//         document.getElementById(id).innerHTML= sentence.substr(0,index);
-//         if(++index === sentence.length){
-//         clearInterval(timer);
-//         }
-//     }, speed);
-// }
+function printText(id, sentence, speed) 
+{
+    var index = 0,
+        timer = setInterval(function() {
+        var char= sentence.charAt(index);
+        if(char === '<') index= sentence.indexOf('>',index);
+        document.getElementById(id).innerHTML= sentence.substr(0,index);
+        if(++index === sentence.length){
+        clearInterval(timer);
+        }
+    }, speed);
+}
 
 
-// printBasicInfo
-// (
-//     "basicInfo",
-//     "<p>My name is <span>Miguel Fortuna</span>. I just graduated from the Ateneo de Manila University and I love designing and developing responsive web pages.</p>",
-//     60
-// );
+printText
+(
+    "name",
+    "<span>MIGUEL ALEJANDRO GRAY FORTUNA</span>",
+    100
+);
+
+printText
+(
+    "role",
+    "<span>Junior Front-End Developer</span>",
+    70
+);
+
+function openInNewTab(url) {
+    var win = window.open(url, '_blank');
+    win.focus();
+}
